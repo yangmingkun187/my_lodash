26162ac70ae1ccad4f92267fd00b5896bb7c5977
JasonYang <jasonyang@outlook.com>
2015-12-18 18:12:41 +0800

    complate array.max

--- app/array/array.js
+++ app/array/array.js
@@ -10,4 +10,34 @@ Array.forEach = function(array, callback) {
   }
 }
 
+Array.max = function(array,callback) {
+  var max;
+  var maxFlag;
+  if(typeof callback === "function") {
+    maxFlag = callback(array[0]);
+    for(var i = 0, len = array.length; i < len; i++) {
+      if(maxFlag < callback(array[i])) {
+        maxFlag = callback(array[i]);
+        max = array[i];
+      }
+    }
+  } else if(typeof callback === 'string') {
+    maxFlag = array[0][callback];
+    for(var i = 0, len = array.length; i < len; i++) {
+      if(maxFlag < array[i][callback]) {
+        maxFlag = array[i][callback];
+        max = array[i];
+      }
+    }
+  } else {
+    max = array[0];
+    for(var i = 0, len = array.length; i < len; i++) {
+      if(array[i] > max) {
+        max = +array[i];
+      }
+    }
+  }
+  return max;
+}
+
 module.exports = Array;
--- test/arrayTest/arrayTest.js
+++ test/arrayTest/arrayTest.js
@@ -3,18 +3,82 @@
 describe('array', function() {
   var _ = require('../../app/array/array.js');
   var array;
-  var testArray = [];
   describe('forEach test', function() {
-    it('test array forEach', function() {
-
+    it('test array list', function() {
+      var testArray = [];
       array = [1,2,3,4,5];
 
       _.forEach(array, function(item) {
         testArray.push(item);
       })
-
       expect(testArray).to.deep.equal(array);
+    });
 
+    it('test array Map1', function() {
+      var testArray = [];
+      array = [{name:'yang',age:18},{name:'ming',age:23},{name:'yang',age:18}];
+
+      _.forEach(array, function(item) {
+        testArray.push(item);
+      })
+      expect(testArray).to.deep.equal(array);
+    });
+
+    it('test array dyadic array;', function() {
+      var testArray = [];
+      array = [{
+        name:'yang',
+        age:18,
+        skills:['js','mocha','chai']
+      },{
+        name:'ming',
+        age:23,
+        skills:['angular','jquery','zepto']
+      }];
+
+      _.forEach(array, function(item) {
+        _.forEach(item.skills,function(skill) {
+          testArray.push(skill);
+        })
+      })
+      expect(testArray).to.deep.equal(['js','mocha','chai','angular','jquery','zepto']);
     })
-  })
+  });
+
+
+  describe('max test', function() {
+    it('should be return max num of array', function() {
+      array = [1,8,3,6,5,2];
+
+      var result = _.max(array);
+
+      expect(result).to.equal(8);
+    });
+
+    it('should be return max of array', function() {
+      array = ['1','5','8','2'];
+
+      var result = _.max(array);
+
+      expect(result).to.equal(8);
+    });
+
+    it('should be return max value of array', function() {
+      array = [{name:'yang',age:18},{name:'ming',age:23},{name:'yang',age:20}];
+
+      var result = _.max(array, function(item) {
+          return item.age;
+      });
+
+      expect(result).to.deep.equal({name:'ming',age:23});
+    });
+
+    it('should be return max item of array', function() {
+      array = [{name:'yang',age:18},{name:'ming',age:23},{name:'yang',age:20}];
+
+      var result = _.max(array, 'age');
+
+      expect(result).to.deep.equal({name:'ming',age:23});
+    });
+  });
 });
